d0842c0d784551d383ee3b58dd41caa179891ec7
jhs851 <jhs851@naver.com>
2021-10-24 21:16:33 +0900

    feat: send-message 컴포넌트 생성
    #33

---
+++ components/SendMessage/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { SendContainer, SubmitContainer, TextareaContainer } from './styled'
+import Textarea from 'components/Textarea'
+import Image from 'components/Image'
+import { useForm } from 'react-hook-form'
+
+type P = {
+  onSubmit: (input: FormInput) => Promise<void>
+}
+
+type FormInput = {
+  message: string
+  asset: string
+}
+
+const SendMessage: React.FC<P> = ({ onSubmit }) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isValid },
+  } = useForm<FormInput>({
+    mode: 'onChange',
+    defaultValues: {
+      message: '',
+      asset: '',
+    },
+  })
+  const submit = async (input: FormInput) => {
+    reset({ message: '' })
+    await onSubmit(input)
+  }
+
+  return (
+    <SendContainer onSubmit={handleSubmit(submit)}>
+      <TextareaContainer>
+        <Textarea
+          placeholder="메시지를 입력하세요.."
+          rows={1}
+          {...register('message', {
+            validate: val => Boolean(val.trim()),
+          })}
+        />
+      </TextareaContainer>
+
+      <SubmitContainer disabled={!isValid} type="submit">
+        <Image src="/icons/send.svg" width={26} height={18} alt="전송" />
+      </SubmitContainer>
+    </SendContainer>
+  )
+}
+
+export default SendMessage
